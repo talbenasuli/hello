60e2b3ac40b5f8676095846cdef3b3d1ee303fdc
tal <tal@Lenovo-PC>
2016-05-05 16:51:28 +0300

    Binary Tree example

--- QuestionsFromCheckPoint/src/BinaryTree.java
+++ QuestionsFromCheckPoint/src/BinaryTree.java
@@ -1,1 +1,35 @@
-shalom
+
+public class BinaryTree 
+{
+	public void insert(Node perent,int key)
+	{
+		if(key < perent.getKey())
+		{
+			if (perent.getLeft() != null) 
+			{
+				insert(perent.getLeft(),key);
+			}
+			else
+			{
+				perent.setLeft(new Node(key));
+				System.out.println("The node have been added\n");
+			}
+		}
+		
+		else if(key > perent.getKey())
+		{
+			if (perent.getRight() != null) 
+			{
+				insert(perent.getRight(),key);
+			}
+			else
+			{
+				perent.setRight(new Node(key));
+				System.out.println("The node have been added\n");
+			}
+		}
+		
+	}
+	
+	
+}
